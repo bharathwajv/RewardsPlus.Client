15a874a7e62cdabac767acde07a9f01a63833b6b
bharathwajv <bharathwaj.vasu@gmail.com>
2022-05-15 22:42:07 +0530

    added logo

--- src/Client.Infrastructure/Preferences/ClientPreference.cs
+++ src/Client.Infrastructure/Preferences/ClientPreference.cs
@@ -9,7 +9,7 @@ public class ClientPreference : IPreference
     public bool IsDrawerOpen { get; set; }
     public string PrimaryColor { get; set; } = CustomColors.Light.Primary;
     public string SecondaryColor { get; set; } = CustomColors.Light.Secondary;
-    public double BorderRadius { get; set; } = 5;
+    public double BorderRadius { get; set; } = 11;
     public string LanguageCode { get; set; } = LocalizationConstants.SupportedLanguages.FirstOrDefault()?.Code ?? "en-US";
     public FshTablePreference TablePreference { get; set; } = new FshTablePreference();
 }
